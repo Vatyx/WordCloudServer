84e575208e4029945da481653ec2e8d9680f1daa
Sahil Dhanju <sahildhanju1@gmail.com>
2016-05-01 03:07:02 -0500

    Added some shit you know

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-var unirest = require('unirest');
+var Nightmare = require('nightmare');
+var nightmare = Nightmare({ show: true })
 
-unirest.post("https://wordcloudservice.p.mashape.com/generate_wc")
-.header("X-Mashape-Key", "eptFiBDjvYmsh54pR00lDRjpkWQrp1nsxx8jsnVbzeMPPX7bt3")
-.header("Content-Type", "application/json")
-.header("Accept", "application/json")
-.send({"f_type":"png","width":800,"height":500,"s_max":"4","s_min":".5","f_min":3,"r_color":"TRUE","r_order":"FALSE","s_fit":"FALSE","fixed_asp":"TRUE","rotate":"TRUE","url":"https://wordcloudapp.herokuapp.com/","remove_words":"white house obama president"})
-.end(function (result) {
-	  console.log(result.status, result.headers, result.body);
-});
+nightmare
+  .goto('http://www.wordclouds.com/')
+  .wait()
+  .click('/html/body/div[3]/div[3]/div/div[1]/div[1]/button')
+  .end()
+  .then(function (result) {
+    console.log(result)
+  })
