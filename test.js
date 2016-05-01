03e11048b9b6526610cea2a8bc573c8e6099db17
Sahil Dhanju <sahildhanju1@gmail.com>
2016-05-01 00:54:42 -0500

    Trying again

--- test.js
+++ test.js
@@ -1,20 +1,10 @@
-var request = require('request')
+var unirest = require('unirest');
 
-var options = {
-  method: 'post',
-  body: {hello: "Happy 24th Birthday to Travis Scott I can tell for real you been working out you look at lot better I can see ur muscle definition I'm happy because if he noticed it's real An article about Thala Movies in India TOday Happy Birthday Thala HBDDearestThalaAjith Happy Sunday Huhu kainggit andaming tao sa Glorieta how I wish andon dn akoo Enjoy po kau dyan ALDUBKiligSaSweetDay es una de las personas ms fuertes que conozco mi amorrrr sabes que te amoooHAPPY BIRTHDAY abravo03 Anyone can make you happy by doing something sweet but only someone special can make you happy without doing anything ExoticLouu happy birthday boo AbdullahFPN where did I mention being happy though Today was my cousins wedding shower  I missed it cuz I had to work but I'm really happy my cuz found a guy that treats her how she deserves happy birthday to one of my"}, // Javascript object
-  json: true, // Use,If you are sending JSON data
-  url: "http://localhost:3000",
-  headers: {
-    // Specify headers, If any
-  }
-}
-
-request(options, function (err, res, body) {
-  if (err) {
-    console.log('Error :', err)
-    return
-  }
-  console.log(' Body :', body)
-
+unirest.post("https://wordcloudservice.p.mashape.com/generate_wc")
+.header("X-Mashape-Key", "eptFiBDjvYmsh54pR00lDRjpkWQrp1nsxx8jsnVbzeMPPX7bt3")
+.header("Content-Type", "application/json")
+.header("Accept", "application/json")
+.send({"f_type":"png","width":800,"height":500,"s_max":"4","s_min":".5","f_min":3,"r_color":"TRUE","r_order":"FALSE","s_fit":"FALSE","fixed_asp":"TRUE","rotate":"TRUE","url":"https://wordcloudapp.herokuapp.com/","remove_words":"white house obama president"})
+.end(function (result) {
+	  console.log(result.status, result.headers, result.body);
 });
